b28a32f5968d9d69965d1551e0c7836a200fdc74
LukasMoyano <moyanomoraleslukas@gmail.com>
2023-12-12 08:56:14 -0500

    Nineth

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,27 @@
+import React from 'react';
+import './styles/home_style.css';
+
 const Home = () => {
   return (
-    // Sección principal que contiene la estructura de la página principal
     <main className="min-[100px]:mt-10 ">
-      {/* Sección de artículos */}
- {/* Título principal */}
- <h1 className="md:w-[50%] text-3xl font-bold mb-4 text-white text-center">
-          <a>Lukas Moyano</a>
-          <span className="text-purple-500"> Visual Artist</span> &
-          <span className="text-purple-500"> Full Stack Web Developer</span>
-        </h1>
-
+<h1 className="md:w-[50%] text-3xl font-bold mb-4 text-white text-center block-effect relative">
+<div className="block-reveal" style={{ "--bc": "white", "--d": ".1s" }}>
+    <a className='text-white text-6xl'>Lukas Moyano</a>
+  </div>
+  <div className="block-reveal" style={{ "--bc": "#4040bf", "--d": ".1s" }}>
+    <span className="text-purple-500"> Visual Artist</span>
+  </div>
+  <div className="block-reveal" style={{ "--bc": "white", "--d": ".1s" }}>
+    <span className="text-white"> &</span>
+  </div>
+  <div className="block-reveal" style={{ "--bc": "#4040bf", "--d": ".1s" }}>
+    <span className="text-purple-500"> Full Stack Web Developer</span>
+  </div>
+</h1>
       <article>
-       
-
-        {/* Sección flexible con diseño de fila en pantallas medianas y grandes */}
         <div className="flex flex-col md:flex-row items-center md:items-start">
-          {/* Sección izquierda con imagen */}
           <section className="md:w-[50%]">
-            {/* Contenedor de la imagen con efecto de escala al hacer hover */}
             <div className="border-withe border-4 rounded-xl bg-[url(/BackgroundHome001.png)] md:w-full bg-cover bg-center relative hover:scale-95 transition-all">
-              {/* Imagen */}
               <img
                 src="../IMG_0440_01.png"
                 alt="Image"
@@ -27,7 +29,6 @@ const Home = () => {
               />
             </div>
           </section>
-
           <section className="md:w-[50%] text-white px-2 p-4 content-around">
             <h2 className="text-2xl font-bold mb-4 text-center">
               Perfect Solution for Your Business
@@ -35,10 +36,9 @@ const Home = () => {
             <p className="">
               Explore a visionary approach tailored to elevate your business.
               From cutting-edge Ecommerce Website Development to captivating
-              multimedia content creation, I can employs diverse methods and
+              multimedia content creation, I can employ diverse methods and
               capabilities to deliver exceptional results.
             </p>
-
             <section className="mt-2 px-2">
               <ul className=" flex flex-wrap p-4 ml-2">
                 <li className="md:w-[50%] mb-2">
